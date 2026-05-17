3efb9ae1ef3ffc0c1036a90b235caa4fd0ee2af9
jeongyun914 <j.yun@sookmyung.ac.kr>
2026-05-18 01:08:01 +0900

    비밀번호길이수정

--- BackEnd/my_project/routes/user.py
+++ BackEnd/my_project/routes/user.py
@@ -61,6 +61,9 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
             status_code=400,
             detail="비밀번호가 너무 깁니다. (최대 72bytes)"
         )
+    print(user.password)
+    print(len(user.password))
+    print(len(user.password.encode("utf-8")))
 
     hashed_password = pwd_context.hash(user.password)
 
