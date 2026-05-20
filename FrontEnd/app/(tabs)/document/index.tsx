818eb7b55fe327b46ca6676a4b738d9cf257426d
lamyoon815 <lamyoon815@gmail.com>
2026-05-20 18:17:35 +0900

    fix: user_id 를 토큰으로

--- FrontEnd/app/(tabs)/document/index.tsx
+++ FrontEnd/app/(tabs)/document/index.tsx
@@ -174,6 +174,7 @@ export default function DocumentScreen() {
           headers: {
             Authorization: `Bearer ${token}`,
             'Content-Type': 'multipart/form-data',
+            
           },
         });
       };
