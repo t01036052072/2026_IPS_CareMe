dfe3105517f66d0a0af39a4b4f2c8a74dc0bbde0
LINAGG1 <leeng060124@gmail.com>
2026-05-26 13:51:41 +0900

    fix: api 주소 변경

--- FrontEnd/api/api.ts
+++ FrontEnd/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://3.27.45.51:8000 ';
+const BASE_URL = 'http://3.27.45.51:8000';
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
