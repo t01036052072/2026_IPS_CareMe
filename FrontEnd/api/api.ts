fac1712f7e767151787ed59b69ba22ff7e292efa
LINAGG1 <leeng060124@gmail.com>
2026-05-26 13:43:12 +0900

    fix: 서버 주소 교체

--- FrontEnd/api/api.ts
+++ FrontEnd/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://52.63.115.68:8000';
+const BASE_URL = 'http://3.27.45.51:8000';
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
